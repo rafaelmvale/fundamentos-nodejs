9cb8c9ddd04ac0d62ba1fa1c02db85cac321e3cd
rafaelmvale <rafael.marques.vale.dev@gmail.com>
2020-04-13 19:23:42 -0300

    desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,10 +1,41 @@
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
 }
+
+const reducer: (balance: Balance, transaction: Transaction) => Balance = (
+  balance,
+  transaction,
+) => {
+  switch (transaction.type) {
+    case 'income':
+      return {
+        ...balance,
+        income: balance.income + transaction.value,
+        total: balance.total + transaction.value,
+      };
+    case 'outcome':
+      return {
+        ...balance,
+        outcome: balance.outcome + transaction.value,
+        total: balance.total - transaction.value,
+      };
+    default:
+      break;
+  }
+  return balance;
+};
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +45,23 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(reducer, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Request): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
